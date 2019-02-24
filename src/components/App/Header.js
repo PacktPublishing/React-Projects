c0c49912a0a638869dc1242f881fd177b0d6cd5c
royderks <hello@hackteam.io>
2019-02-24 14:45:36 +0100

    Splitted App component into different components

---
+++ src/components/App/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = ({logo}) => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </header>
+);
+
+export default Header;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from '../components/App/Header';
 import logo from '../logo.svg';
 import './App.css';
 
@@ -6,20 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header logo={logo} />
       </div>
     );
   }
