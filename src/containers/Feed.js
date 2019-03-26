28bde394b4e7f4587c5caba4f647c1365c246486
royderks <10717410+royderks@users.noreply.github.com>
2019-03-27 00:02:49 +0100

    Small changes to initial application

--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import Card from '../components/Feed/Card';
+import Card from '../components/shared/Card';
 
 const FeedWrapper = styled.div`
   display: flex;
