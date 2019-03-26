4e251f5c0a4ae450fb0285158abc84396554b57e
royderks <10717410+royderks@users.noreply.github.com>
2019-03-26 20:37:05 +0100

    setted up initial application

---
+++ src/containers/Feed.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Card from '../components/Feed/Card';
+
+const FeedWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  margin: 5%;
+`;
+
+const Loading = styled.div`
+  text-align: center;
+`;
+
+class Feed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loading: 'Loading...'
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const data = await fetch('https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow');
+      const dataJSON = await data.json();
+
+      if (dataJSON) {
+        this.setState({
+          data: dataJSON,
+          loading: false
+        })
+      }
+    } catch(error) {
+      this.setState({
+       loading: error.message
+     })
+    }
+  }
+
+  render() {
+    const { data, loading } = this.state;
+
+    if (loading) {
+      return <Loading>{loading}</Loading>
+    }
+
+    return (
+      <FeedWrapper>
+        { data.items.map((item) => <Card key={item.question_id} data={item} />) }
+      </FeedWrapper>
+    );
+  }
+}
+
+export default Feed;
