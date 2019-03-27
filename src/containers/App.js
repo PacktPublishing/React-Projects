e365142208a3f32a4322f22db122fa32e5a7b6b8
royderks <10717410+royderks@users.noreply.github.com>
2019-03-28 00:13:35 +0100

    Add dynamic routing with query strings to support pagination

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from '../components/App/Header';
 import Feed from './Feed';
 import Question from './Question';
@@ -29,8 +29,11 @@ class App extends Component {
             <AppWrapper>
             <Header />
             <Router>
-              <Route exact path="/" component={Feed} />
-              <Route path="/questions/:id" component={Question} />
+              <Switch>
+                <Route exact path="/" component={Feed} />
+                <Route path="/questions/:id" component={Question} />
+                <Route path="/questions" component={Feed} />
+              </Switch>
             </Router>
           </AppWrapper>
         </>
--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import queryString from 'query-string'
 import { Link } from "react-router-dom";
 import Card from '../components/shared/Card';
 
@@ -19,18 +20,34 @@ const CardLink = styled(Link)`
   color: inherit;
 `;
 
+const PaginationBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Button = styled(Link)`
+  padding: 1%;
+  background: lightBlue;
+  color: white;
+  text-decoration: none
+  border-radius: 5px;
+`
+
 class Feed extends Component {
   constructor(props) {
     super(props);
+    const query = queryString.parse(this.props.location.search);
     this.state = {
       data: [],
+      page: (query.page) ? parseInt(query.page) : 1,
       loading: 'Loading...'
     };
   }
 
-  async componentDidMount() {
+  async fetchAPI(page) {
     try {
-      const data = await fetch('https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow');
+      const data = await fetch(`https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow${(page) ? `&page=${page}` : ''}`);
       const dataJSON = await data.json();
 
       if (dataJSON) {
@@ -43,11 +60,27 @@ class Feed extends Component {
       this.setState({
        loading: error.message
      })
+   }
+  }
+
+  componentDidMount() {
+    const { page } = this.state;
+    this.fetchAPI(page);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.location.search !== this.props.location.search) {
+      const query = queryString.parse(this.props.location.search);
+      this.setState(
+        { page: parseInt(query.page) },
+        () => this.fetchAPI(this.state.page)
+      );
     }
   }
 
   render() {
-    const { data, loading } = this.state;
+    const { data, page, loading } = this.state;
+    const { match } = this.props;
 
     if (loading) {
       return <Loading>{loading}</Loading>
@@ -56,10 +89,14 @@ class Feed extends Component {
     return (
       <FeedWrapper>
         { data.items.map((item) => (
-          <CardLink to={`/questions/${item.question_id}`}>
-            <Card key={item.question_id} data={item} />
+          <CardLink key={item.question_id} to={`/questions/${item.question_id}`}>
+            <Card data={item} />
           </CardLink>
         )) }
+        <PaginationBar>
+          { (page > 1) && <Button to={`${match.url}?page=${page - 1}`}>Previous</Button> }
+          { (data.has_more) && <Button to={`${match.url}?page=${page + 1}`}>Next</Button> }
+        </PaginationBar>
       </FeedWrapper>
     );
   }
