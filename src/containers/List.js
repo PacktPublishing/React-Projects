4e6de2afcb38e204f8839661d6e8c4591d187297
royderks <10717410+royderks@users.noreply.github.com>
2019-04-13 20:17:29 +0200

    Updated naming convention for functional components

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import Card from '../components/List/Card';
+import Card from '../components/Card/Card';
 
 class List extends Component {
   constructor() {
