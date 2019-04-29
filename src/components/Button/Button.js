6725db32cc49c3bc7825fa0d6e53b6e220d69fd9
royderks <10717410+royderks@users.noreply.github.com>
2019-04-29 23:27:11 +0200

    Created initial application

---
+++ src/components/Button/Button.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: transparent;
+  color: white;
+  padding: 10px;
+  line-height: 2;
+  border-radius: 5px;
+  font-weight: bold;
+  border: 4px solid white;
+  font-size: inherit;
+  cursor: pointer;
+`;
+
+export default Button;
---
+++ src/context/GlobalContext.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HotelsContextProvider from './HotelsContextProvider';
+import ReviewsContextProvider from './ReviewsContextProvider';
+
+const GlobalContext = ({children}) => {
+  return (
+    <HotelsContextProvider>
+      <ReviewsContextProvider>
+        {children}
+      </ReviewsContextProvider>
+    </HotelsContextProvider>
+  );
+};
+
+export default GlobalContext;
