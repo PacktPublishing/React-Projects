1720224303b7cf43f4e0510f0287bce7df18e283
royderks <10717410+royderks@users.noreply.github.com>
2019-06-27 22:54:13 +0200

    Initial game

--- Components/Slot/Slot.js
+++ Components/Slot/Slot.js
@@ -3,15 +3,13 @@ import { Text, View, TouchableOpacity, Dimensions } from 'react-native';
 import styled from 'styled-components/native';
 
 const Slot = ({ index, checked, handleOnPress }) => (
-  <SlotWrapper checked={checked} onPress={() => !checked && handleOnPress(index)}>
-    {checked && <SlotIcon><Text>{`player: ${checked}`}</Text></SlotIcon>}
-  </SlotWrapper>
+  <SlotWrapper checked={checked} onPress={() => !checked && handleOnPress(index)} />
 )
 
 const SlotWrapper = styled(TouchableOpacity)`
   width: ${Dimensions.get('window').width * 0.3};
   height: ${Dimensions.get('window').width * 0.3};
-  background-color: ${({ checked }) => checked ? 'pink' : '#000'};
+  background-color: ${({ checked }) => checked ? (checked === 1 ? 'blue' : 'green') : 'grey'};
   border: 1px solid #fff;
 `
 
--- Screens/Leaderboard.js
+++ Screens/Leaderboard.js
@@ -1,24 +1,53 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import styled from 'styled-components/native';
 import { AppContext } from '../context/AppContext';
 
-const Leaderboard = ({ navigation }) => {
+const Leaderboard = () => {
   const {scores} = React.useContext(AppContext)
 
-  console.log(scores)
   return (
-  <View style={styles.container}>
-    <Text>Open up App.js to start working on your app!</Text>
-  </View>
+    <LeaderBoardWrapper>
+      <LeaderBoardTitle>Scores</LeaderBoardTitle>
+      <PlayerScore>
+        <PlayerScoreLabel>Player 1</PlayerScoreLabel>
+        <PlayerScoreNumber>{ scores.player1 }</PlayerScoreNumber>
+      </PlayerScore>
+      <PlayerScore>
+        <PlayerScoreLabel>Player 2</PlayerScoreLabel>
+        <PlayerScoreNumber>{ scores.player2 }</PlayerScoreNumber>
+      </PlayerScore>
+    </LeaderBoardWrapper>
 )};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const LeaderBoardWrapper = styled(View)`
+  flex: 1
+  align-items: stretch;
+  margin: 60px 0;
+`
+
+const LeaderBoardTitle = styled(Text)`
+  padding: 40px 0;
+  width: 100%;
+  text-align: center;
+  font-size: 48px;
+`
+
+const PlayerScore = styled(View)`
+  padding: 40px 0;
+`
+
+const PlayerScoreLabel = styled(Text)`
+  padding-bottom: 20px;
+  width: 100%;
+  text-align: center;
+  font-size: 28px;
+`
+const PlayerScoreNumber = styled(Text)`
+  width: 100%;
+  text-align: center;
+  font-size: 48px;
+  font-weight: bold;
+`
 
 export default Leaderboard;
