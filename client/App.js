8116da234884fca7524e8bddfdd65bd9b922e705
royderks <10717410+royderks@users.noreply.github.com>
2019-07-09 22:32:06 +0200

    Added the basic init for apollo, without the websocket

--- client/App.js
+++ client/App.js
@@ -11,13 +11,29 @@
 // export default App;
 
 import React from "react";
-import AppContextProvider from './context/AppContext';
-import AppContainer from './AppContainer';
+import AppContextProvider from "./context/AppContext";
+import AppContainer from "./AppContainer";
+
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "react-apollo";
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "http://localhost:4000/graphql"
+  }),
+  cache
+});
 
 const App = () => (
-  <AppContextProvider>
-    <AppContainer />
-  </AppContextProvider>
-)
+  <ApolloProvider client={client}>
+    <AppContextProvider>
+      <AppContainer />
+    </AppContextProvider>
+  </ApolloProvider>
+);
 
 export default App;
