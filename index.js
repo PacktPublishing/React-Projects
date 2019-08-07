810bbbf019907da4b8c01108e2a444597702681f
royderks <10717410+royderks@users.noreply.github.com>
2019-08-07 23:40:43 +0200

    Initial code

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { AppRegistry } from "react-360";
+import App from "./src/App";
+
+const virtual_reality = () => <App />;
+
+AppRegistry.registerComponent("virtual_reality", () => virtual_reality);
