da75bf5e8ef90d32a6e39784d2859e825e9968f4
royderks <10717410+royderks@users.noreply.github.com>
2019-09-02 00:49:41 +0200

    Updates to chapter to include authentication

---
+++ client/src/components/Checkout/Checkout.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "../Button/Button";
+
+const Checkout = () => {
+  return (
+    <CheckoutWrapper>
+      <p>This is the checkout, press the button below to complete:</p>
+      <Button color="royalBlue">Complete checkout</Button>
+      )}
+    </CheckoutWrapper>
+  );
+};
+
+const CheckoutWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  margin: 2% 5%;
+`;
+
+export default Checkout;
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,25 @@
+const { AuthenticationError } = require("apollo-server");
 const faker = require("faker");
+const JsonWebToken = require("jsonwebtoken");
+const Bcrypt = require("bcryptjs");
+
+const jwtSecret = "34%%##@#FGFKFL";
+
+const isTokenValid = token => {
+  const bearerToken = token.split(" ");
+
+  if (bearerToken) {
+    return JsonWebToken.verify(bearerToken[1], jwtSecret, error => {
+      if (error) {
+        return false;
+      }
+
+      return true;
+    });
+  }
+
+  return false;
+};
 
 const mockCategory = () => ({
   id: faker.random.number,
@@ -19,7 +40,8 @@ const mockProduct = (id = false) => ({
 
 let cart = {
   total: 0,
-  products: []
+  products: [],
+  complete: false
 };
 
 const resolvers = {
@@ -34,11 +56,51 @@ const resolvers = {
   Mutation: {
     addToCart: (_, { id }) => {
       cart = {
+        ...cart,
         total: cart.total + 1,
         products: [...cart.products, mockProduct(id)]
       };
 
       return cart;
+    },
+    completeCart: (_, {}, { token }) => {
+      const isValid = token ? isTokenValid(token) : false;
+
+      if (isValid) {
+        cart = {
+          ...cart,
+          complete: true
+        };
+
+        return cart;
+      }
+      throw new AuthenticationError(
+        "Please provide (valid) authentication details"
+      );
+    },
+    loginUser: async (_, { userName, password }) => {
+      let isValid;
+      const user = {
+        userName: "test",
+        password: "$2b$10$5dwsS5snIRlKu8ka5r7z0eoRyQVAsOtAZHkPJuSx.agOWjchXhSum"
+      };
+
+      if (userName === user.userName) {
+        isValid = await Bcrypt.compareSync(password, user.password);
+      }
+
+      if (isValid) {
+        const token = JsonWebToken.sign({ user: user.userName }, jwtSecret, {
+          expiresIn: 3600
+        });
+        return {
+          userName,
+          token
+        };
+      }
+      throw new AuthenticationError(
+        "Please provide (valid) authentication details"
+      );
     }
   }
 };
