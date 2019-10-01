16646c400644495b4754ccf17359d0d0ffb1ba00
royderks <10717410+royderks@users.noreply.github.com>
2019-10-01 21:08:40 +0200

    Use the expo vector icons package

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import Ionicons from '@expo/vector-icons';
 import {
   createStackNavigator,
   createBottomTabNavigator,
