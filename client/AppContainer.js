1b9ad99cd2242600a95927105ed06992a3c2e7b9
royderks <10717410+royderks@users.noreply.github.com>
2019-12-17 21:53:22 +0100

    Updated the code for the KeyBoardAvoidingView

--- client/AppContainer.js
+++ client/AppContainer.js
@@ -1,67 +1,64 @@
-import React from "react";
-import { Platform } from "react-native";
+import React from 'react';
+import { Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import {
-  createSwitchNavigator,
-  createAppContainer
-} from 'react-navigation';
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import Posts from "./Screens/Posts";
-import Post from "./Screens/Post";
-import Settings from "./Screens/Settings";
-import Login from "./Screens/Login";
-import AuthLoading from "./Screens/AuthLoading";
-import AddPost from "./Screens/AddPost";
-import Notifications from "./Screens/Notifications";
+import Posts from './Screens/Posts';
+import Post from './Screens/Post';
+import Settings from './Screens/Settings';
+import Login from './Screens/Login';
+import AuthLoading from './Screens/AuthLoading';
+import AddPost from './Screens/AddPost';
+import Notifications from './Screens/Notifications';
 
 const PostsStack = createStackNavigator({
   Posts: {
     screen: Posts,
-    navigationOptions: { title: "All Posts" }
+    navigationOptions: { title: 'All Posts' },
   },
   Post: {
     screen: Post,
-    navigationOptions: { title: "Post" }
-  }
+    navigationOptions: { title: 'Post' },
+  },
 });
 
 const NotificationsStack = createStackNavigator({
   Notifications: {
     screen: Notifications,
-    navigationOptions: { title: "Notifications" }
-  }
-})
+    navigationOptions: { title: 'Notifications' },
+  },
+});
 
 const TabNavigator = createBottomTabNavigator(
   {
     Posts: PostsStack,
     Notifications: NotificationsStack,
-    Settings
+    Settings,
   },
   {
-    initialRouteName: "Posts",
+    initialRouteName: 'Posts',
     defaultNavigationOptions: ({ navigation }) => ({
       tabBarIcon: ({ tintColor }) => {
         const { routeName } = navigation.state;
 
         let iconName;
-        if (routeName === "Posts") {
-          iconName = `${Platform.OS === "ios" ? "ios" : "md"}-home`;
-        } else if (routeName === "Settings") {
-          iconName = `${Platform.OS === "ios" ? "ios" : "md"}-settings`;
-        } else if (routeName === "Notifications") {
-          iconName = `${Platform.OS === "ios" ? "ios" : "md"}-notifications`;
+        if (routeName === 'Posts') {
+          iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-home`;
+        } else if (routeName === 'Settings') {
+          iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-settings`;
+        } else if (routeName === 'Notifications') {
+          iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-notifications`;
         }
 
         return <Ionicons name={iconName} size={20} color={tintColor} />;
       },
       tabBarOptions: {
-        activeTintColor: "orange",
-        inactiveTintColor: "#556"
-      }
-    })
-  }
+        activeTintColor: 'orange',
+        inactiveTintColor: '#556',
+      },
+    }),
+  },
 );
 
 const SwitchNavigator = createSwitchNavigator(
@@ -69,12 +66,12 @@ const SwitchNavigator = createSwitchNavigator(
     Main: TabNavigator,
     Login,
     AuthLoading,
-    AddPost
+    AddPost,
   },
   {
-    mode: "modal",
-    initialRouteName: "AuthLoading"
-  }
+    mode: 'modal',
+    initialRouteName: 'AuthLoading',
+  },
 );
 
 export default createAppContainer(SwitchNavigator);
